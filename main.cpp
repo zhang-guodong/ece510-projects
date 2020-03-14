98b880d9138a410f7b075a138f23cbcc7898a1f5
zhangguodong <gd.zhang@live.com>
2020-03-15 00:03:03 +0800

    achieve question [2]

--- main.cpp
+++ main.cpp
@@ -5,11 +5,15 @@
 using namespace std;
 
 int main() {
+    clock_t start, finish;
+    start = clock();
     srand((unsigned) time(NULL));
     int i = rand();
     cout << i << endl;
     cout << "The random integer is ";
     if (i <= 100) cout << "not ";
-    cout << "greater than 100.";
+    cout << "greater than 100." << endl;
+    finish = clock();
+    cout << finish - start;
     return 0;
 }
